bfceb283a6ba96a886457df95b44c141ee203010
Micah Silverman <micah@afitnerd.com>
2025-03-16 18:55:02 -0400

    attack

--- dist/index.js
+++ dist/index.js
@@ -77,20 +77,16 @@ async function updateFeatures(token) {
   
 }
 
-async function run() {
-  try {
-    await updateFeatures();
-    // `who-to-greet` input defined in action metadata file
-    const nameToGreet = core.getInput('who-to-greet');
-    console.log(`Hello ${nameToGreet}!`);
-    const time = (new Date()).toTimeString();
-    core.setOutput("time", time);
-  } catch (error) {
-    core.setFailed(error.message);
-  }  
-};
-
-run();
+try {
+  updateFeatures();
+  // `who-to-greet` input defined in action metadata file
+  const nameToGreet = core.getInput('who-to-greet');
+  console.log(`Hello ${nameToGreet}!`);
+  const time = (new Date()).toTimeString();
+  core.setOutput("time", time);
+} catch (error) {
+  core.setFailed(error.message);
+}  
 module.exports = __webpack_exports__;
 /******/ })()
 ;
