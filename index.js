fef6e8660f74270397e0dbc6f97995d38d2a7b72
Micah Silverman <micah@afitnerd.com>
2025-03-16 18:31:26 -0400

    run

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 
-try {
-  // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput('who-to-greet');
-  console.log(`Hello ${nameToGreet}!`);
-  const time = (new Date()).toTimeString();
-  core.setOutput("time", time);
-} catch (error) {
-  core.setFailed(error.message);
+export async function run() {
+  try {
+    // `who-to-greet` input defined in action metadata file
+    const nameToGreet = core.getInput('who-to-greet');
+    console.log(`Hello ${nameToGreet}!`);
+    const time = (new Date()).toTimeString();
+    core.setOutput("time", time);
+  } catch (error) {
+    core.setFailed(error.message);
+  }  
 }
+
+run();
